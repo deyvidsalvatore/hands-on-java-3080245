31be3784c0452989a41f79169c7d7ca8e6b836a8
Deyvid Santos <115676414+deyvidsalvatore@users.noreply.github.com>
2024-01-25 21:50:59 +0000

    Retrieving Customer s' Balance on Account

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -2,7 +2,7 @@ package bank;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException; 
+import java.sql.SQLException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
@@ -26,27 +26,49 @@ public class DataSource {
     String sql = "select * from customers where username = ?";
     Customer customer = null;
 
-    try(Connection connection = connect();
-      PreparedStatement statement = connection.prepareStatement(sql)
-    ) {
+    try (Connection connection = connect();
+        PreparedStatement statement = connection.prepareStatement(sql)) {
       statement.setString(1, username);
       try (ResultSet resultSet = statement.executeQuery()) {
         customer = new Customer(
-          resultSet.getInt("id"), 
-          resultSet.getString("name"), 
-          resultSet.getString("username"), 
-          resultSet.getString("password"), 
-          resultSet.getInt("account_id"));
+            resultSet.getInt("id"),
+            resultSet.getString("name"),
+            resultSet.getString("username"),
+            resultSet.getString("password"),
+            resultSet.getInt("account_id"));
       }
-    } catch(SQLException e) {
+    } catch (SQLException e) {
       e.printStackTrace();
     }
 
     return customer;
   }
 
+  public static Account getAccount(int accountId) {
+    String sql = "select * from accounts where id = ?";
+    Account account = null;
+
+    try (Connection connection = connect();
+        PreparedStatement statement = connection.prepareStatement(sql)) {
+      statement.setInt(1, accountId);
+      try (ResultSet resultSet = statement.executeQuery()) {
+        account = new Account(
+            resultSet.getInt("id"),
+            resultSet.getString("type"),
+            resultSet.getDouble("balance"));
+      }
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+    return account;
+  }
+
   public static void main(String[] args) {
     Customer customer = getCustomer("twest8o@friendfeed.com");
+    Account account = getAccount(customer.getAccountId());
     System.out.println(customer.getName());
+    System.out.println(account.getBalance());
   }
 }
